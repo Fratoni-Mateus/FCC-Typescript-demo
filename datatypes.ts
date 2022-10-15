92001f1c34d488c4b538995c325aa5e8c637d701
Mateus Fratoni <Mateus.Fratonisouza@gmail.com>
2022-10-15 16:50:44 -0300

    Initial commit

---
+++ datatypes.ts
@@ -0,0 +1,37 @@
+let lName: string;
+
+lName = "Souza";
+
+let newName = lName.toUpperCase();
+
+console.log(newName);
+
+let age: number;
+
+age = 25;
+age = 25.5;
+
+let dob = "25";
+
+let result = parseInt(dob);
+
+let isValid: boolean = false;
+
+console.log(isValid);
+
+let empList: string[];
+
+empList = ["Souza", "Souza1", "Souza2"];
+
+let numList: Array<number>;
+
+numList = [1, 2, 3, 4, 5];
+
+let results = numList.filter((num) => num > 2);
+
+let num = numList.find((num) => num === 2);
+
+console.log(results);
+console.log(num);
+
+// lname = 10;
